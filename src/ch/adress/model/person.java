a8ed1b471b7e28f5f27d40773f60d3b12fde7b0d
Sur0vy <Sur0vy@yandex.ru>
2015-06-14 22:21:08 +0300

    начало 3-го занятия

---
+++ src/ch/adress/model/person.java
@@ -0,0 +1,7 @@
+package ch.adress.model;
+
+/**
+ * Created by Dmitry on 05.06.2015.
+ */
+public class person {
+}
---
+++ src/ch/adress/view/PersonOverviewController.java
@@ -0,0 +1,7 @@
+package ch.adress.view;
+
+/**
+ * Created by Dmitry on 10.06.2015.
+ */
+public class PersonOverviewController {
+}
